adfa371be4d614cfb981bcbf019f7eb024d101e0
Mburiah <bykieswagix5@gmail.com>
2019-12-03 17:02:55 +0300

    create an array with quotes objects

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes: Quote[] = [
+    new Quote(1, 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live ',
+    'Rick Cook, The Wizardry Compiled', 'Linus Torvalds',
+    new Date(2019, 12, 3), 0, 0),
+    new Quote(2, 'Good programmers write code that humans can understand', 'Martin Fowler', 'Larry Niven',
+    new Date(2019, 12, 1), 0, 0),
+    new Quote(3, 'I\'m not a great programmer; I\'m just a good programmer with great habits..', 'Why The Lucky Stiff', 'Kent Beck',
+    new Date(2019 , 12, 3), 0, 0),
+  ];
   constructor() { }
 
   ngOnInit() {
